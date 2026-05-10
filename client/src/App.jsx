e302afbac1774dd97040e3a3aab2d77cadeaa0df
kazimdabusayed <kazimohammedabusayed@gmail.com>
2026-05-10 19:23:23 +0600

    Fix routing in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,34 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+
+function ProtectedRoute({ children }) {
+	const token = localStorage.getItem('token');
+
+	return token ? children : <Navigate to="/signin" />;
+}
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Home />} />
+
+				<Route path="/signin" element={<Signin />} />
+
+				<Route path="/signup" element={<Signup />} />
+
+				<Route
+					path="/dashboard"
+					element={
+						<ProtectedRoute>
+							<Dashboard />
+						</ProtectedRoute>
+					}
+				/>
 			</Routes>
 		</BrowserRouter>
 	);
